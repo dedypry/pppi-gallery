3ffbc3edf972fc8501f60013e3f8fec3e81055d2
dedy priyatna <57609978+dedypry@users.noreply.github.com>
2025-08-23 22:33:28 +0700

    update

--- configs/cors.js
+++ configs/cors.js
@@ -2,10 +2,12 @@ require('dotenv/config')
 const allowedOrigins =
   process.env.NODE_ENV === "production"
     ? [
-        "https://dpn-pppi.org",
-        "https://admin.dpn-pppi.org",
-        "https://api.dpn-pppi.org",
-      ]
+      "https://dpn-pppi.org",
+      "https://admin.dpn-pppi.org",
+      "https://api.dpn-pppi.org",
+      'https://90home.id', 
+      'https://admin.90home.id'
+    ]
     : ["http://localhost:5174", "http://localhost:5173"];
 
 module.exports = {
